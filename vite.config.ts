35558c5d9ad248fa59044343958b9f8933bc17d3
Rob Harman <mailrobharman@gmail.com>
2026-02-16 19:49:27 -0600

    fix(build): add @shared path alias to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@shared": path.resolve(__dirname, "./shared"),
     },
   },
   server: {
